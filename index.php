1a4fb058a5aa79ebe6e74c16f43b7c26aca2e90f
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-20 10:00:40 -0300

    Create arquivo index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="" method="post">
+            Email: <input type="text" name="email" value=""> <br>
+            Senha: <input type="text" name="senha" value=""> <br>
+            <button>Cadastrar</button>
+        </form>
+    </body>
+</html>
