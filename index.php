9ec062a59cf760863c5ae8e76ce00c7c7d891998
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-20 10:31:35 -0300

    Realizado teste de validação de dados

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
         <form action="" method="post">
             Email: <input type="text" name="email" value=""> <br>
             Senha: <input type="text" name="senha" value=""> <br>
+            <button type="submit" name="logar">Entrar</button>
             <button>Cadastrar</button>
         </form>
 
-        <?php
+    </body>
+</html>
 
+<?php
+        if(isset($_POST['logar'])){
         $email = $_POST["email"];
         $senha = $_POST["senha"];
 
         echo "Olá, o e-amil cadastrado foi ".$email.", e a senha é ".$senha;
+    }
         ?>
-    </body>
-</html>
