eb6d1aa124c630c1e643386f490005ed527626d3
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-21 10:46:24 -0300

    Criado comandos da class Cntrollers

--- Controllers/usuario_controller.php
+++ Controllers/usuario_controller.php
@@ -1,6 +1,18 @@
 <?php
-class UsuarioController{
+$path = $_SERVER['DOCUMENT_ROOT'].'/ecommerce/';
+include_once($path.'/Models/Usuario.php');
 
-}
+    class UsuarioController{
+        public function login($objUsuario){
+            echo $objUsuario->getEmail();
 
+            if($objUsuario->getEmail() == null strlen($objUsuario->getEmail()) > 100){
+                return "Email inválido";
+            }
+
+            if($objUsuario->getSenha() == null || strlen($objUsuario->getSenha()) > 100){
+                return "Senha inválida";
+            }
+        }
+    }
  ?>
